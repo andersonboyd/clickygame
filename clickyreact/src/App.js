cd04881adddf085986502c2144c2aac734f5d4a5
Anderson <andersonswaimboyd@gmail.com>
2019-07-30 18:00:37 -0500

    added readme

--- clickyreact/src/App.js
+++ clickyreact/src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import chars from "./chars.json";
+import "./App.css";
 
 class App extends Component {
   state = {
