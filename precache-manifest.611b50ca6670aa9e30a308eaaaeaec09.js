e7ec5e299e513973275042b7801078f3bb2ef3f8
Anderson <andersonswaimboyd@gmail.com>
2019-07-30 14:33:27 -0500

    Updates

---
+++ precache-manifest.611b50ca6670aa9e30a308eaaaeaec09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e16b5714f3904225318eadeacbdefa11",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "0435ad69ab70618d8e4c",
+    "url": "/clickygame/static/css/main.63c2d96c.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "0435ad69ab70618d8e4c",
+    "url": "/clickygame/static/js/main.a8f1ca18.chunk.js"
+  },
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "43d42aeb265f673b7735334a39b059eb",
+    "url": "/clickygame/static/media/Textile Regular.43d42aeb.ttf"
+  }
+]);
